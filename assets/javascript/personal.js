29b310a562f638f6c2c1cbc42fcf5db9bfe1fa65
unknown <Valarie.Regas@gmail.com>
2017-10-08 12:13:27 -0400

    added firebase info

--- assets/javascript/personal.js
+++ assets/javascript/personal.js
@@ -6,11 +6,72 @@ $(document).ready(function(){
       $('.carousel').carousel();
 
 
-      $("#textarea1").keyup(function(event){
-    		if(event.keyCode == 13){
-        		$("#textarea1").click();
-    		}
-		});
+      // Initialize Firebase
+	  var config = {
+	    apiKey: "AIzaSyDHqWcl97ZbHgWAseXMmBkuS-GQN4KkIyY",
+	    authDomain: "contac-f92f8.firebaseapp.com",
+	    databaseURL: "https://contac-f92f8.firebaseio.com",
+	    projectId: "contac-f92f8",
+	    storageBucket: "contac-f92f8.appspot.com",
+	    messagingSenderId: "169312956245"
+	  };
+	  
+	  firebase.initializeApp(config);
+
+	  // global variable declaration
+
+	  var name;
+	  var emailAdd;
+	  var message;
+
+	  var database = firebase.database();
+
+	  // function for clicking submit
+
+	  $("#sub-btn").on("click", function() {
+
+	  	//prevent refresh
+	  	event.preventDefault();
+
+	  	//define variables
+	  	name = $("#icon_prefix").val().trim();
+	  	emailAdd = $("#email").val().trim();
+	  	message = $("#textarea1").val().trim();
+
+	  	//logs to make sure the above worked
+	  	console.log("Name: " + name);
+	  	console.log("Email:" + emailAdd);
+	  	console.log("Message:" + message);
+
+	  	// Firebase Info
+        database.ref('/Contacts').push({
+            name: name,
+            emailAdd: emailAdd,
+            message: message,
+            dateAdded: firebase.database.ServerValue.TIMESTAMP
+        // Close of Firebase Info
+        });
+
+        // Clear previous inputs
+        function clearFields() {
+            $(".info").val("");
+        }
+
+        clearFields();
+
+
+	  	//close of on click submit btn
+	  });
+
+
+
+
+
+  //     $("#textarea1").keyup(function(event){
+  //   		if(event.keyCode == 13){
+  //       		$("#textarea1").click();
+  //   		}
+		// });
 
 
 

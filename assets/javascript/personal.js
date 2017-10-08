74a7bd4e03a5804c28113f85610062d0746ca3de
unknown <Valarie.Regas@gmail.com>
2017-10-08 10:17:34 -0400

    portfolio mostly complete, initial contact form

--- assets/javascript/personal.js
+++ assets/javascript/personal.js
@@ -1,4 +1,23 @@
 $(document).ready(function(){
+	// starts parallax for background
       $('.parallax').parallax();
+
+      // starts carousel effect on portfolio
       $('.carousel').carousel();
+
+
+      $("#textarea1").keyup(function(event){
+    		if(event.keyCode == 13){
+        		$("#textarea1").click();
+    		}
+		});
+
+
+
+
+
+
+
+
+      //close of doc ready
     });
